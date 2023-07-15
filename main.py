480987db2dddf342a2684c976a6867b82dab7244
Renato <renato_da_costa2000@yahoo.com.br>
2023-07-15 01:50:49 -0300

    feat: ways to hide the data

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel # Não precisa pip install, todos os atributos e data serão herdados daqui
 
+class CoffeeIn(BaseModel):
+    secret_id: int            # Não aparece na url -> http://127.0.0.1:8000/coffee/
+    nome:str
+    tipo: str
+    descricao: Optional[str] = None
+
 class Coffee(BaseModel):
     nome:str
     tipo: str
@@ -13,24 +19,27 @@ app = FastAPI()
 async def quero_cafeeee():
     return { 'quero': 'cafééééé!!!!'}
 
-# @app.post('/coffee/')
-# async def make_coffee(coffee: Coffee):
+
+# @app.post('/coffee/', response_model=Coffee)
+# async def make_coffee(coffee: CoffeeIn):
 #     return coffee
 
-@app.post('/coffee/{nivel}')
-async def make_coffee(coffee: Coffee, nivel: int, value: bool):
-    return {**coffee.model_dump(), 'nivel': nivel, 'value': value}
+# @app.post('/coffee/', response_model=Coffee, response_model_include={'tipo'})
+# async def make_coffee(coffee: CoffeeIn):
+#     return coffee
 
 '''
-    return {'coffee': coffee, 'nivel': nivel, 'value': value}
-
 {
-  "coffee": {
-    "nome": "3 corações",
-    "tipo": "forte",
-    "descricao": "acorda urso hibernando!!Cuidado!!"
-  },
-  "nivel": 1,
-  "value": true
+  "tipo": "fromHell"
+}
+'''
+@app.post('/coffee/', response_model=Coffee, response_model_exclude={'tipo'})
+async def make_coffee(coffee: CoffeeIn):
+    return coffee
+
+'''
+{
+  "nome": "3 corações",
+  "descricao": "burns like hell!!"
 }
 '''
